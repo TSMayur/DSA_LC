5791c77563436cd98d741dc8f0fb51d96b93c696
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-26 19:36:48 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (88.17%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int n = digits.size();
-
-        for (int i = n - 1; i >= 0; --i) {
-            digits[i]++;
-            
-            // If the digit is less than 10, there's no carry, so we're done.
-            if (digits[i] < 10) {
+        
+        for(int i = digits.size()-1; i>= 0; i--){
+            if(digits[i] <9){
+                digits[i]++;
                 return digits;
             }
-            
-            // Otherwise, there is a carry. Set the digit to 0 and continue.
             digits[i] = 0;
         }
-
-        // If the loop completes, it means the original number was all 9s.
-        // We need to insert a 1 at the beginning.
         digits.insert(digits.begin(), 1);
         return digits;
     }
