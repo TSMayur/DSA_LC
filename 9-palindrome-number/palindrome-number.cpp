825cd19767fff21887d066eabd3d377d780104a6
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-27 20:38:08 +0530

    Time: 7 ms (15.83%) | Memory: 10.8 MB (15.21%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        // Negative numbers and numbers ending with 0 (but not 0 itself) are not palindromes
-        if (x < 0 || (x % 10 == 0 && x != 0))
-            return false;
-
-        int reversedHalf = 0;
-        // Only reverse half of the number to prevent overflow and optimize
-        while (x > reversedHalf) {
-            reversedHalf = reversedHalf * 10 + x % 10;
-            x /= 10;
+        string str = to_string(x);
+        int l = str.size()-1;
+        for(int i = 0 ;i<str.size()/2 ; i++){
+            if(str[i] != str[l-i]) return false;
         }
-
-        // If the number has an odd number of digits, we get rid of the middle digit using /10
-        return (x == reversedHalf || x == reversedHalf / 10);
+        return true;
     }
 };
