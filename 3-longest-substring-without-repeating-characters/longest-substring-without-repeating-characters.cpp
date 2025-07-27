d43173f4e5805fdfb71ee448b9a7a7fff24bec6d
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-27 16:46:10 +0530

    Time: 3 ms (83.53%) | Memory: 11.3 MB (90.89%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> mp;
-        int l = 0, res = 0;
-        
-        for (int r = 0; r < s.size(); r++) {
-            if (mp.find(s[r]) != mp.end()) {
-                l = max(mp[s[r]] + 1, l);
-            }
-            mp[s[r]] = r;
-            res = max(res, r - l + 1);
-        }
-        return res;
+  int lengthOfLongestSubstring(string s) {
+    std::array<int, 256> store;
+    store.fill(-1);
+    int start = 0, end = 0, n = s.size(), res = 0;
+    for (int end = 0; end < n; ++ end) {
+      char c = s[end];
+      if (store[c] != -1) {
+        start = std::max(start, store[c]);
+      }
+      res = std::max(res, end - start + 1);
+      store[c] = end + 1;
     }
+    return res;
+  }
 };
