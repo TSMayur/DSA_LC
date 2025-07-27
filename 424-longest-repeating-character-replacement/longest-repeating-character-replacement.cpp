59645a2d48710cf8478af08b290416bb1e10677f
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-27 17:00:40 +0530

    Time: 11 ms (29.13%) | Memory: 11.1 MB (7.61%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
-    int characterReplacement(string s, int k) {
-        int n=s.size();
-        int left=0, right=0, maxFreq=0, maxLen=0;
-        vector<int> mpp(26,0);
-        while(right<n){
-            mpp[s[right]-'A']++;
-            maxFreq=max(maxFreq,mpp[s[right]-'A']);
-            if(right-left+1 - maxFreq > k){
-                mpp[s[left]-'A']--;
-                left++;
+    int characterReplacement(std::string s, int k) {
+        int res = 0;
+        unordered_set<char> charSet(s.begin(), s.end());
+
+        for (char c : charSet) {
+            int count = 0, l = 0;
+            for (int r = 0; r < s.size(); r++) {
+                if (s[r] == c) {
+                    count++;
+                }
+
+                while ((r - l + 1) - count > k) {
+                    if (s[l] == c) {
+                        count--;
+                    }
+                    l++;
+                }
+
+                res = std::max(res, r - l + 1);
             }
-            maxLen=max(maxLen,right-left+1);
-            right++;
         }
-        return maxLen;
+        return res;
     }
 };
