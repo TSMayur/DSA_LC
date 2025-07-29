35afc4a45e2e8fd27a8477fd6c27f6d2f8977b5c
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-29 08:51:53 +0530

    Time: 3 ms (91.70%) | Memory: 15.3 MB (26.32%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -17,25 +17,25 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
-        unordered_map<Node*, Node*> oldToCopy;
-        oldToCopy[nullptr] = nullptr;
+        unordered_map<Node*, Node*> o;
+        o[nullptr] = nullptr;
 
         Node* cur = head;
         while (cur != nullptr) {
-            if (oldToCopy.find(cur) == oldToCopy.end()) {
-                oldToCopy[cur] = new Node(0);
+            if (o.find(cur) == o.end()) {
+                o[cur] = new Node(0);
             }
-            oldToCopy[cur]->val = cur->val;
-            if (oldToCopy.find(cur->next) == oldToCopy.end()) {
-                oldToCopy[cur->next] = new Node(0);
+            o[cur]->val = cur->val;
+            if (o.find(cur->next) == o.end()) {
+                o[cur->next] = new Node(0);
             }
-            oldToCopy[cur]->next = oldToCopy[cur->next];
-            if (oldToCopy.find(cur->random) == oldToCopy.end()) {
-                oldToCopy[cur->random] = new Node(0);
+            o[cur]->next = o[cur->next];
+            if (o.find(cur->random) == o.end()) {
+                o[cur->random] = new Node(0);
             }
-            oldToCopy[cur]->random = oldToCopy[cur->random];
+            o[cur]->random = o[cur->random];
             cur = cur->next;
         }
-        return oldToCopy[head];
+        return o[head];
     }
 };
