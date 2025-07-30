e016c06658157c4ac36421e9938641b02c057695
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-30 19:55:38 +0530

    Time: 3 ms (77.61%) | Memory: 13.7 MB (84.65%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
+   bool isVowel(char ch){
+    return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
+   }
     long long countVowels(string word) {
-        long long tv = 0;
-        long long n = word.length();
-
-        for (long long i = 0; i < n; ++i) {
-            char c = word[i];
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                // This vowel contributes to (i + 1) * (n - i) substrings.
-                tv += (i + 1) * (n - i);
-            }
+      int n=word.size();
+      long long total=0;
+      for(int i=0;i<n;i++){
+        if(isVowel(word[i])){
+            long long contribution=(long long)(i+1)*(n-i);
+            total+=contribution;
         }
-        
-        return tv;
+      }
+      return total;
     }
 };
