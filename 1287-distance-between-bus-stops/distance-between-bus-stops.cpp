798f047e7da14a88f9e1eda2a1c4f48508220d43
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-30 20:06:54 +0530

    Time: 3 ms (63.17%) | Memory: 12.5 MB (50.82%) - LeetSync

--- 1287-distance-between-bus-stops/distance-between-bus-stops.cpp
+++ 1287-distance-between-bus-stops/distance-between-bus-stops.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
-        
-        int total_sum = accumulate(distance.begin(), distance.end(), 0);
+        int n = distance.size();
+        int total = accumulate(distance.begin(), distance.end(), 0);
+        int dis1 = 0, dis2 = 0;
 
-        if (start > destination) {
-            swap(start, destination);
-        }
-        
-        int clockwise_dist = 0;
-        for (int i = start; i < destination; ++i) {
-            clockwise_dist += distance[i];
-        }
-        
-        int counter_clockwise_dist = total_sum - clockwise_dist;
+        if (start > destination) swap(start, destination);
 
-        return min(clockwise_dist, counter_clockwise_dist);
+        for (int i = start; i < destination; i++) {
+            dis1 += distance[i];
+        }
+
+        dis2 = total - dis1;
+
+        return min(dis1, dis2);
     }
 };
