b357518b562826aab16e7530d0036ff9ecf364c8
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-30 19:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (99.10%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,27 +1,58 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        // Paired values and symbols, sorted from largest to smallest
-        vector<pair<int, string>> values = {
-            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
-            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"},
-            {1, "I"}
-        };
+        string ans {""};
+        int M =  num / 1000,
+            C = (num / 100) % 10,
+            X = (num / 10)  % 10,
+            I = num % 10;
+        if (M != 0) {                      // 1234
+            for (int i = M; i > 0; --i)    // ^
+                ans.push_back('M');
+        }
 
-        string result = "";
-        
-        for (const auto& pair : values) {
-            int value = pair.first;
-            string symbol = pair.second;
-            
-            // While the number is large enough, append the symbol and subtract the value
-            while (num >= value) {
-                result += symbol;
-                num -= value;
+        if (C != 0) {                           // 1234
+            if      (C == 9) ans.append("CM");  //  ^
+            else if (C == 4) ans.append("CD");
+            else if (C >= 5) {
+                ans.push_back('D');
+                for (int i = C; i > 5; --i)
+                    ans.push_back('C');
+            }
+            else {
+                for (int i = C; i > 0; --i)
+                    ans.push_back('C');
             }
         }
-        
-        return result;
+
+        if (X != 0) {                           // 1234
+            if      (X == 9) ans.append("XC");  //   ^
+            else if (X == 4) ans.append("XL");
+            else if (X >= 5) {
+                ans.push_back('L');
+                for (int i = X; i > 5; --i)
+                    ans.push_back('X');
+            }
+            else {
+                for (int i = X; i > 0; --i)
+                    ans.push_back('X');
+            }
+        }
+
+        if (I != 0) {                           // 1234
+            if      (I == 9) ans.append("IX");  //    ^
+            else if (I == 4) ans.append("IV");
+            else if (I >= 5) {
+                ans.push_back('V');
+                for (int i = I; i > 5; --i)
+                    ans.push_back('I');
+            }
+            else {
+                for (int i = I; i > 0; --i)
+                    ans.push_back('I');
+            }
+        }
+
+        return ans;
     }
 };
