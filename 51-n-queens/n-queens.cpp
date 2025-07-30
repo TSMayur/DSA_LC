dab4e2143cc411cf5e1d2cfa4389d24498ed03c2
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-30 19:16:43 +0530

    Time: 1 ms (80.89%) | Memory: 10.2 MB (55.05%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,52 +1,43 @@
 class Solution {
-private:
-    vector<bool> cols;
-    vector<bool> pos_diag;
-    vector<bool> neg_diag;
-    vector<vector<string>> result;
-    int board_size;
+public:
+    bool isSafe(vector<string>& board, int row, int col, int n) {
+        // Column check
+        for (int i = 0; i < row; i++)
+            if (board[i][col] == 'Q')
+                return false;
 
-    void backtrack(int row, vector<string>& board) {
-        if (row == board_size) {
-            result.push_back(board);
+        // Left diagonal
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 'Q')
+                return false;
+
+        // Right diagonal
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++)
+            if (board[i][j] == 'Q')
+                return false;
+
+        return true;
+    }
+
+    void nQueens(vector<string>& board, int row, int n, vector<vector<string>>& ans) {
+        if (row == n) {
+            ans.push_back(board);
             return;
         }
 
-        for (int col = 0; col < board_size; ++col) {
-            int pos_diag_idx = row - col + board_size - 1;
-            int neg_diag_idx = row + col;
-
-            if (cols[col] || pos_diag[pos_diag_idx] || neg_diag[neg_diag_idx]) {
-                continue;
+        for (int j = 0; j < n; j++) {
+            if (isSafe(board, row, j, n)) {
+                board[row][j] = 'Q';             // Place queen
+                nQueens(board, row + 1, n, ans); // Go to next row
+                board[row][j] = '.';             // Backtrack
             }
-
-            // Place the queen and update state
-            board[row][col] = 'Q';
-            cols[col] = true;
-            pos_diag[pos_diag_idx] = true;
-            neg_diag[neg_diag_idx] = true;
-
-            // Recurse to the next row
-            backtrack(row + 1, board);
-
-            // Backtrack: remove the queen and reset state
-            board[row][col] = '.';
-            cols[col] = false;
-            pos_diag[pos_diag_idx] = false;
-            neg_diag[neg_diag_idx] = false;
         }
     }
 
-public:
     vector<vector<string>> solveNQueens(int n) {
-        board_size = n;
-        vector<string> board(n, string(n, '.'));
-        
-        cols.resize(n, false);
-        pos_diag.resize(2 * n - 1, false);
-        neg_diag.resize(2 * n - 1, false);
-        
-        backtrack(0, board);
-        return result;
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.')); // Initialize empty board
+        nQueens(board, 0, n, ans);
+        return ans;
     }
 };
