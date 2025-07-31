c7b32cff2d1975cb7198fac54d94bfbfbb1f852a
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-07-31 20:33:15 +0530

    Time: 8 ms (71.40%) | Memory: 12.9 MB (74.62%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.cpp
+++ 2825-minimize-string-length/minimize-string-length.cpp
@@ -1,16 +1,9 @@
-#include <string>
-#include <unordered_set>
-
-using namespace std;
-
 class Solution {
 public:
-    int minimizedStringLength(string s) {
-        // Create a set of characters from the string.
-        // The set will automatically handle duplicates, storing only one of each character.
-        unordered_set<char> unique_chars(s.begin(), s.end());
-        
-        // The size of the set is the number of unique characters.
-        return unique_chars.size();
+    int minimizedStringLength(string S){
+        int f[26]={0},count=0;
+        for(const char & c : S) f[c-'a']++;
+        for(int i=0;i<26;i++) if(f[i]>=1) count++;
+        return count;
     }
 };
