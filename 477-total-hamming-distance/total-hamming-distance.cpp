c9aed2577a0609883f96d26589055b4361190fb1
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-08-01 23:02:05 +0530

    Time: 8 ms (73.86%) | Memory: 23 MB (16.46%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,30 +1,18 @@
-#include <vector>
-
-using namespace std;
 
 class Solution {
 public:
-    int totalHammingDistance(vector<int>& nums) {
-        int total_distance = 0;
-        int n = nums.size();
-
-        // Iterate through each bit position from 0 to 30
-        for (int i = 0; i < 31; ++i) {
-            int set_bits_count = 0;
-            
-            // For the current bit 'i', count how many numbers have it set
-            for (int num : nums) {
-                if ((num >> i) & 1) {
-                    set_bits_count++;
-                }
-            }
-            
-            int zero_bits_count = n - set_bits_count;
-            
-            // Add the contribution of this bit to the total distance
-            total_distance += set_bits_count * zero_bits_count;
+int totalHammingDistance(const vector<int>& nums) { 
+    int n = nums.size();
+    int total = 0;
+    // For each bit position (0 to 31)
+    for (int bit = 0; bit < 32; ++bit) {
+        int countOnes = 0;
+        for (int num : nums) {
+            countOnes += (num >> bit) & 1;
         }
-        
-        return total_distance;
+        int countZeros = n - countOnes;
+        total += countOnes * countZeros;
     }
+    return total;
+}
 };
