305bf29a08e3c559d9f9bb7a595d4ed63ed00557
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-08-02 21:23:44 +0530

    Time: 7 ms (50.07%) | Memory: 26 MB (99.34%) - LeetSync

---
+++ 661-image-smoother/image-smoother.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& img) {
+        int m = img.size();
+        int n = img[0].size();
+        
+        vector<vector<int>> smoothed_img(m, vector<int>(n));
+
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                int sum = 0;
+                int count = 0;
+
+                // Iterate through the 3x3 neighborhood
+                for (int i = r - 1; i <= r + 1; ++i) {
+                    for (int j = c - 1; j <= c + 1; ++j) {
+                        // Check if the neighbor is within the grid boundaries
+                        if (i >= 0 && i < m && j >= 0 && j < n) {
+                            sum += img[i][j];
+                            count++;
+                        }
+                    }
+                }
+                smoothed_img[r][c] = sum / count;
+            }
+        }
+
+        return smoothed_img;
+    }
+};
