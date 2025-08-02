5fb8c208f6171166bc7e6be83de5f9459bae907e
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-08-02 21:10:54 +0530

    Time: 0 ms (100.00%) | Memory: 35 MB (89.55%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.cpp
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
-        int min1 = *min_element(nums1.begin(), nums1.end());
-        int min2 = *min_element(nums2.begin(), nums2.end());
-        return min2 - min1;
+        int m1 = *min_element(nums1.begin(), nums1.end());
+        int m2 = *min_element(nums2.begin(), nums2.end());
+        return m2 - m1;
     }
 };
