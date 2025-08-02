7242d7c959b0b062454fac320ffc7e76db54fbe3
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-08-02 21:28:42 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (90.36%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,20 +1,15 @@
-#include <vector>
-#include <utility>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        int w = 0; // Marks the end of the even number section
+        int wp = 0; // Marks the end of the even number section
 
-        for (int r = 0; r < nums.size(); ++r) {
+        for (int rp = 0; rp < nums.size(); ++rp) {
             // If we find an even number
-            if (nums[r] % 2 == 0) {
+            if (nums[rp] % 2 == 0) {
                 // Swap it into the even section
-                swap(nums[w], nums[r]);
+                swap(nums[wp], nums[rp]);
                 // Grow the even section's boundary
-                w++;
+                wp++;
             }
         }
         
