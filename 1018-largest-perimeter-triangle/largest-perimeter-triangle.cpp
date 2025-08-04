3c29a378a8ee539bcd8874341f07baeef502def8
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-08-04 14:34:11 +0530

    Time: 14 ms (17.78%) | Memory: 25.7 MB (45.77%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
-        // Sort the array to easily apply the triangle inequality theorem.
         sort(nums.begin(), nums.end());
-
-        // Iterate from the end, checking triplets.
         for (int i = nums.size() - 1; i >= 2; --i) {
-            // If a <= b <= c, we only need to check if a + b > c.
             if (nums[i-2] + nums[i-1] > nums[i]) {
-                // This is the first valid triangle from the largest sides,
-                // so it must have the largest perimeter.
                 return nums[i-2] + nums[i-1] + nums[i];
             }
         }
-        
-        // No valid triangle could be formed.
         return 0;
     }
 };
