d4b7b23755a9354f16dd2774f43a55de602fc075
Mayur TS <156050291+TSMayur@users.noreply.github.com>
2025-08-04 14:36:10 +0530

    Time: 6 ms (51.95%) | Memory: 25.6 MB (75.57%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -5,14 +5,14 @@ public:
         sort(nums.begin(), nums.end(), greater<int>());
         if (n < 3)
             return 0;
-        int  res;
+        int  r;
         for(int i=0;i<n-2;i++){
         for (int j = i;j<i+3;j++) {
             if ((nums[i] + nums[i+1] > nums[i+2]) &&
                 (nums[i+1] + nums[i+2] > nums[i]) &&
                 (nums[i] + nums[i+2] > nums[i+1])) {
-                res = nums[i] + nums[i+1] + nums[i+2];
-                return res;
+                r = nums[i] + nums[i+1] + nums[i+2];
+                return r;
             }
         }
         }
